c602154ed669536a351c5b8095368067067ddcb0
jaquelineamara <jaquelineamara5@gmail.com>
2022-03-22 22:36:52 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, SOBRENOME;
+
+Console.WriteLine("Digite seu primeniro nome:");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("Digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+SOBRENOME=sobrenome.ToUpper();
+
+Console.WriteLine ($"Nome Completo: {nome} {sobrenome}"! );
+Console.WriteLine($"Nome de catalogo: {SOBRENOME} {nome}"! );
